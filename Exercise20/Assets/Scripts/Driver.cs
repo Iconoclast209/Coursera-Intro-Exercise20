b3528ae7b3164dae0e2c0a59f8186b6bb1e7f13e
asmith <mrpresidentsmith@gmail.com>
2019-04-14 20:28:32 -0700

    Exercise completed.

---
+++ Exercise20/Assets/Scripts/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]
+    const float MoveUnitsPerSecond = 5f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 newPosition = transform.position;
+
+        if (horizontalMovement != 0)
+        {
+            //add horizontal movement
+            
+            newPosition.x += horizontalMovement * MoveUnitsPerSecond * Time.deltaTime;
+        }
+
+        if(verticalMovement != 0)
+        {
+            //add vertical movement
+            newPosition.y += verticalMovement * MoveUnitsPerSecond * Time.deltaTime;
+        }
+
+        //move the object
+        transform.position = newPosition;
+    }
+}
